fbf36a80786d1dfed9f3600b65864b513ba54076
mannyaalonso <mannyaalonso@gmail.com>
2023-05-15 09:05:47 -0400

    added link to home on logo

--- client/src/components/Nav.jsx
+++ client/src/components/Nav.jsx
@@ -1,11 +1,14 @@
 import logo from '../assets/images/logo.png'
+import { Link } from 'react-router-dom'
 
 //575
 const Nav = () => {
   return (
     <div className="h-[81px] xl:h-[104px] sticky top-0 z-50 bg-[#003057]">
       <div className="h-[81px] xl:h-[104px] xl:w-[105px] sm:ml-4 bg-white w-[80px] flex items-center justify-center">
-        <img className="h-8 xl:h-10" src={logo} alt="" />
+        <Link to={'/'}>
+          <img className="h-8 xl:h-10" src={logo} alt="" />
+        </Link>
       </div>
     </div>
   )
